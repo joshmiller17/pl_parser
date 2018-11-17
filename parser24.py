bcb77456656b46514514fc118d8c260246f1ed33
Josh Miller <miller.josh@husky.neu.edu>
2018-11-17 16:02:01 -0500

    new examples

--- parser24.py
+++ parser24.py
@@ -5,17 +5,57 @@ Quirk 24 Parser
 Author: Josh Miller
 """
 
+SPACE = 0x20
+LF = 0xa
+CR = 0xd
+COMMENT = "//" # followed by LF or CR
+ASSIGNOP = "="
+OROP = "||"
+ANDOP = "&&"
+RELOPS = ["==", "!=", "<", "<=", ">", ">="]
+ADDOPS = ["+", "-"]
+MULOPS = ["*", "/"]
+UNOPS = ["!", "-"]
+illegal = False
+
+# TODO
+#  - tab characters are illegal
+
+
 def run(input, output):
 	import os
+	ast = []
 	
 	with open(input, 'r') as file:
 		pass
 		
 		
 	with open(output, 'w') as file:
-		pass
+		if illegal:
+			file.write("(illegal)")
+		else:
+			file.write(ast_to_string(ast, "", 0))
 
 
+def ast_to_string(ast, out, indent_level):
+	if indent_level == 0:
+		out.append("(program\n")
+	indent = indent_level + 1
+	for e in ast:
+		if hasattr(next, "__len__") # check if we need to recurse
+			out.append(ast_to_string(e, out, indent))
+		else:
+			# TODO 
+			print("Not sure what to do here, e is not array: " + e)
+	
+	
+	
+	out.append(")")
+	return out
+			
+			
+			
+			
 def main():
 	import argparse
 	prog_desc = "Quirk 24 parser by Josh Miller"

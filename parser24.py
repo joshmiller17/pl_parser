34a580cf1997047ac453652c0bbdcedc5d5740e6
Josh Miller <miller.josh@husky.neu.edu>
2018-11-17 15:32:35 -0500

    workspace setup

--- parser24.py
+++ parser24.py
@@ -5,7 +5,26 @@ Quirk 24 Parser
 Author: Josh Miller
 """
 
+def run(input, output):
+	import os
+	
+	with open(input, 'r') as file:
+		pass
+		
+		
+	with open(output, 'w') as file:
+		pass
 
+
+def main():
+	import argparse
+	prog_desc = "Quirk 24 parser by Josh Miller"
+	parser = argparse.ArgumentParser(description=prog_desc)
+	parser.add_argument('input', help="Input file name")
+	parser.add_argument('output', help="Output file name")
+	args = parser.parse_args()
+	run(args.input, args.output)
+	
 
 if '__main__' == __name__:
 	main()
